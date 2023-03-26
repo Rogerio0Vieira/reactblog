cfc778f956b5fd6e8bedd243bb3a7f23ad03cda9
Rogerio Vieira <rogeriovbh@gmail.com>
2023-03-25 22:56:01 -0300

    Inicia componente Home

---
+++ reactblog/src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { HomeWrapper, Title } from "./styles";
+
+const Home = () => {
+  return (
+    <HomeWrapper>
+      <Title>Bem-vindo ao meu blog!</Title>
+    </HomeWrapper>
+  );
+};
+
+export default Home;
---
+++ reactblog/src/components/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 24px;
+`;
